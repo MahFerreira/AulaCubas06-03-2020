1d4dfeef880791d14753fc96c4c9aef94756c65c
Matheus <mahglow@gmail.com>
2020-03-06 20:17:14 -0300

    teste1

---
+++ HelloMundoJava.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva a descrição da classe HelloMundoJava aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class HelloMundoJava
+{
+    public static void main(String[] args){
+    System.out.println("meu primeiro codigo em Java");
+    
+    }
+}
